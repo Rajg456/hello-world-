bfceaa8cbbb4bb2978c227c2477986d0a2e1f2c1
Rajg456 <109383893+Rajg456@users.noreply.github.com>
2022-07-23 09:07:27 +0530

    Create Practical 3_2.cpp

---
+++ Practical 3_2.cpp
@@ -0,0 +1,17 @@
+#include<iostream.h>
+#include<conio.h>
+#include<iomanio.h>
+
+int m=11;
+int msin()
+{
+int m=15;
+float PI=3.1437595;
+clrscr();
+cout<"setfill('*')<<setw(10);
+cout<<m<<endl;
+cout<<::m+m<<endl;
+cout<<setprecision (2)<<PI;
+getch();
+return 0;
+}
