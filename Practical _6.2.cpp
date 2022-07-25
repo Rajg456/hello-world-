ffb2aaa6dfe67a15fe4838a1714b99947ebe845d
Rajg456 <109383893+Rajg456@users.noreply.github.com>
2022-07-25 18:03:13 +0530

    Create Practical _6.2.cpp

---
+++ Practical _6.2.cpp
@@ -0,0 +1,30 @@
+#include<iostream.h>
+#include<conio.h>
+class dis{
+private:
+int feet,inches;
+public:
+void get(){
+cout<<"Enter feet";
+cin>>feet 
+cout<<"Enter inches";
+cin>>inches 
+}
+void disp(),{
+cout<<"feet="<<feet;
+cout<<"inches="<<inches;
+}
+void add(dis D,dis D){
+feet=D1.feet+D2.feet;
+inches=D1.inches+D2.inches;
+feet=feet+(inches/12);
+inches=inches%12;
+}};
+void main(){
+Dist.d1,d2,d3;
+clrscr();
+d1.get();
+d2.get();
+d3.add(d1,d2)
+d3.disp();
+}
