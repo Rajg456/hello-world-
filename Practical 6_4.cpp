21f060eb951718a99eed1ebbfdc0bc19859f448b
Rajg456 <109383893+Rajg456@users.noreply.github.com>
2022-07-25 18:17:17 +0530

    Create Practical 6_4.cpp

---
+++ Practical 6_4.cpp
@@ -0,0 +1,35 @@
+#include<iostream.h>
+#include<conio.h>
+class student {
+private:
+int en_r,M1,M2,M3;
+char.name[20],cource[10];
+public:
+void get(){
+cout<<"Enter Enrollment number";
+cin>>en_r
+cout<<"Enter name";
+cin>>name 
+cout<<"Enter sub 3 marks";
+cin>>M1,>>M2,>>M3;
+}
+void sum(){
+int M
+M.M1+M2+M3;
+cout<<"Total marks is"<<m;}
+void disp(){
+cout<<"your enrollment number is<<en_r;
+cout<<"your name is<<name;
+cout<<"your course is<<course;
+sum();
+};
+void main(){
+student s1,s2;
+clrscr();
+s1.get();
+s1.disp();
+s2.get();
+s2.disp();
+getch();
+return 0;
+}
