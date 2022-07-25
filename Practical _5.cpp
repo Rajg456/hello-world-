d58625639437f2125d54d4fe23fa043de3e448aa
Rajg456 <109383893+Rajg456@users.noreply.github.com>
2022-07-25 17:05:07 +0530

    Create Practical _5.cpp

---
+++ Practical _5.cpp
@@ -0,0 +1,28 @@
+#include<iostream.h>
+#include<conio.h>
+struct student 
+{
+int r_no;
+char name[5];
+int age;
+char br[5]
+};
+int main()
+{
+student s;
+clrscr();
+cout<<"enter name:";
+cin>>s.name
+cout<<"enter roll no:";
+cin<<s.r_no
+cout<<"enter age:";
+cin>>s.age
+cout<<"enter branch:";
+cin>>s.br
+cout<<"Display student details"<<endl;
+cout<<"r_no:"<<s.r_no<<endl;
+cout<<"name:"<<s.name<<endl;
+cout<<"br;"<<s.br<<endl;
+getch();
+return 0;
+}
